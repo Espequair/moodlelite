470d62cca34c11e2c2a5e63411daf20a89e4eff1
Arthur <arthur.kluytmans@epita.fr>
2022-01-11 16:18:35 +0100

    [ADD] added dependece to pom and service class

--- lib/src/main/java/com/mti/Service.java
+++
@@ -1,5 +0,0 @@
-package com.mti;
-
-public class Service {
-    @GET 
-}
---
+++ services/src/main/java/com.mti/StudentService.java
@@ -0,0 +1,9 @@
+package com.mti;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.List;
+
+@ApplicationScoped
+public class StudentService {
+    private List<Student> students;
+}
