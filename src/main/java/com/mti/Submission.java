5666adb18e4fb7c82e90e23587017538ce2cc684
Arthur <arthur.kluytmans@epita.fr>
2022-01-11 12:36:40 +0100

    [ADD] added constructor to submission class

--- src/main/java/com/mti/Submission.java
+++ src/main/java/com/mti/Submission.java
@@ -10,4 +10,16 @@ public class Submission {
     private LocalDateTime closing;
     private HashMap<String, HashMap<String, Work>> workList;
 
+    public Submission (String name,
+            String gitPattern,
+            LocalDateTime opening,
+            LocalDateTime closing,
+            HashMap<String, HashMap<String, Work>> workList) {
+
+        this.gitPattern = gitPattern;
+        this.opening = opening;
+        this.closing = closing;
+        this.workList = workList;
+    }
+
 }
