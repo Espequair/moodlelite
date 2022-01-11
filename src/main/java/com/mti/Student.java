37b436db793ae5d68f78611801ba223fe15fa852
Romain Cahuet <romain.cahuet@epita.fr>
2022-01-11 15:28:17 +0100

    Added Student Class

---
+++ src/main/java/com/mti/Student.java
@@ -0,0 +1,11 @@
+package com.mti;
+
+public class Student {
+    public final String login;
+    public final short graduation_year;
+
+    public Student(String login, short graduation_year){
+        this.login = login;
+        this.graduation_year = graduation_year;
+    }
+}
