5ee93c19709e6356ae46367fe820e74dced5fd6d
Romain Cahuet <romain.cahuet@epita.fr>
2022-01-11 12:24:17 +0100

    Updated Activity to add the hash

--- src/main/java/com/mti/Activity.java
+++ src/main/java/com/mti/Activity.java
@@ -1,6 +1,8 @@
 package com.mti;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class Activity {
     private String name;
@@ -9,6 +11,8 @@ public class Activity {
     private LocalDateTime closing;
     private String managerLogin;
     private String gitPattern;
+    HashMap<String, Submission> submissionList;
+    HashSet<String> studentSet;
 
     public Activity(
         String name,
@@ -16,13 +20,19 @@ public class Activity {
         LocalDateTime opening, 
         LocalDateTime closing, 
         String managerLogin, 
-        String gitPattern){
+        String gitPattern,
+        HashMap<String, Submission> submissionList,
+        HashSet<String> studentList
+        )
+        {
             this.name = name;
             this.slug = slug;
             this.opening = opening;
             this.closing = closing;
             this.managerLogin = managerLogin;
             this.gitPattern = gitPattern;
+            this.submissionList = submissionList;
+            this.studentSet = studentSet;
         }
-        
+
 }
